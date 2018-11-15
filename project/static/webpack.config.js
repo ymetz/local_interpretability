0a7d8acd63103b4194a9bb05d1ce77fd2a56e593
Yannick Metz <yannick.metz@uni-konstanz.de>
2018-11-15 15:44:59 +0100

    skeleton for web app (Flask+React+Webpack+Babel)

---
+++ project/static/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const config = {
+    entry:  __dirname + '/js/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
+};
+module.exports = config;
