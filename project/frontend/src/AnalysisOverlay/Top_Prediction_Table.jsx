dd6a9b27158d7b0170e9bc005bcea68fe37d36c9
Yannick Metz <yannick.metz@uni-konstanz.de>
2019-01-09 13:06:06 +0100

    Top Prediction Table

---
+++ project/frontend/src/AnalysisOverlay/Top_Prediction_Table.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const addLabel = (data,id_to_label) => {
+    const outData = []
+    data.forEach(function(d){
+        outData.push({'class' : d.class, 'class_name': id_to_label[d.class], 'score':d.score.toPrecision(3)});
+    })
+    return outData;
+}
+
+const topPredictionTable = (props) => {
+    const data = addLabel(props.data, props.id_to_label);
+
+        const columns = [{
+            dataField: 'class',
+            text: 'Class ID'
+        },
+        {
+            dataField: 'class_name',
+            text: 'Class Name'
+        },
+        {
+            dataField: 'score',
+            text: 'Class Probability',
+            sort: true
+    }]
+
+    const defaultSorted = [{
+        dataField: 'score',
+        order: 'desc'
+    }];
+
+    const selectRow = {
+        mode: 'radio',
+        clickToSelect: true,
+        hideSelectColumn: true,
+        bgColor: '#adebad',
+        onSelect: (row, isSelect, rowIndex, e) => {
+            props.onSelect(row.class);
+        }
+    }
+
+    return(
+    <BootstrapTable 
+        keyField='class' 
+        data={ data } 
+        columns ={ columns } 
+        bordered={ false } 
+        defaultSorted={defaultSorted}
+        selectRow={selectRow}
+        striped
+        hover />
+    )
+}
+
+export default topPredictionTable;
